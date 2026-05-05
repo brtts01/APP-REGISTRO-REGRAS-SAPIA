8e88f652fb2e0f8d32f8aecb0eccc47d99ae98fc
Tiago Brittes <brtts.07@gmail.com>
2026-05-05 09:08:50 -0400

    Add files via upload

---
+++ preload.js
@@ -0,0 +1,11 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('api', {
+  getUsername:     ()              => ipcRenderer.invoke('get-username'),
+  getConfig:       ()              => ipcRenderer.invoke('get-config'),
+  escolherPasta:   ()              => ipcRenderer.invoke('escolher-pasta'),
+  lerHistorico:    ()              => ipcRenderer.invoke('ler-historico'),
+  salvarRegistro:  (registro)      => ipcRenderer.invoke('salvar-registro', registro),
+  deletarRegistro: (id)            => ipcRenderer.invoke('deletar-registro', id),
+  gerarPdf:        (htmlConteudo)  => ipcRenderer.invoke('gerar-pdf', htmlConteudo),
+});
